059318c8a2423e0f756d2c1ed2606e8290afbb95
HarshitTiwari-20 <ht337378@gmail.com>
2025-04-04 10:28:21 +0530

    Initial change

---
+++ components/Hero.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Spotlight } from "./ui/Spotlight";
+
+const Hero = () => {
+  return (
+    <div className="pd-20 pt-36">
+      <div>
+        <Spotlight
+          className="top-40 -lest-10 md:-left-32 md:-top-20 h-screen"
+          fill="white"
+        />
+        <Spotlight
+          className="top-10 left-full h-[80vh] w-[50vw]"
+          fill="purple"
+        />
+        <Spotlight className="top-28 left-80 h-[80vh] w-[50vw]" fill="blue" />
+      </div>
+
+      <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black"></div>
+      <p className="relative z-20 bg-gradient-to-b from-neutral-200 to-neutral-500 bg-clip-text py-8 text-4xl font-bold text-transparent sm:text-7xl">
+        Backgrounds
+      </p>
+    </div>
+  );
+};
+
+export default Hero;
